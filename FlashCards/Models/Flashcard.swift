869397f7175e7d8b4c1ad213070c7ec322c950e9
Nechaev S <{ID}+{username}@users.noreply.github.com>
2021-09-12 13:44:07 +0300

    base model created

--- FlashCards/Models/Flashcard.swift
+++ FlashCards/Models/Flashcard.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Word {
+    let enWord: String
+    var ruTranslation: String
+    var learnStatus: Bool
+    
+    var enRuTranslation: String {
+        "\(enWord) - \(ruTranslation)"
+    }
+    
+}
+
+extension Word {
+    static func getDictionary() -> [Word] {
+        var wordsList: [Word] = []
+        
+        for rawWord in DataManager.shared.words {
+            let wordData = Word(enWord: rawWord.0, ruTranslation: rawWord.1, learnStatus: rawWord.2)
+            wordsList.append(wordData)
+        }
+        
+        return wordsList
+    }
+}
+
+extension Word: Comparable {
+    static func < (lhs: Word, rhs: Word) -> Bool {
+        lhs.enWord < rhs.enWord
+    }
+}
