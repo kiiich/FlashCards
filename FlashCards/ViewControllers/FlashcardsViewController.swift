1e5478a18f93b4cb463afdc3a8d7b5f6de457fbc
KIIICH <KIIICH>
2021-09-12 12:46:05 +0300

    Layout flashcards viewer

--- FlashCards/ViewControllers/FlashcardsViewController.swift
+++ FlashCards/ViewControllers/FlashcardsViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class FlashcardsViewController: UIViewController {
 
+    @IBOutlet weak var showAnswerButton: UIButton!
+    @IBOutlet weak var wordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+    }
+    
 }
