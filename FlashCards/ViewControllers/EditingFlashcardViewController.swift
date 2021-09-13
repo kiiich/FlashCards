e5f187234dc380de77f43001f6a0f53b484ca6b9
moysWeekend <a.b.burtsev@gmail.com>
2021-09-13 21:34:56 +0300

    add outlets & methods without cade

--- FlashCards/ViewControllers/EditingFlashcardViewController.swift
+++ FlashCards/ViewControllers/EditingFlashcardViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class EditingFlashcardViewController: UIViewController {
-
+    
+    @IBOutlet var englishWordTF: UITextField!
+    @IBOutlet var russianWordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func studiedSwitcher() {
     }
-    */
-
+    
+    
+    @IBAction func saveButtonPressed() {
+    }
+    
 }
