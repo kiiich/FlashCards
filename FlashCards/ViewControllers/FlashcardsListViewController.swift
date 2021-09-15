d9d469b3725b05649b90f38f509818d3b62c8775
artem <arte@kb.ri>
2021-09-16 01:05:06 +0300

    add transition EditingFlashcardViewController

--- FlashCards/ViewControllers/FlashcardsListViewController.swift
+++ FlashCards/ViewControllers/FlashcardsListViewController.swift
@@ -11,6 +11,11 @@ class FlashcardsListViewController: UITableViewController {
 
     var flashcards: [Flashcard]!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         flashcards.count
     }
@@ -25,10 +30,21 @@ class FlashcardsListViewController: UITableViewController {
         
         content.text = card.enWord
         content.secondaryText = card.ruTranslation
+        content.image = UIImage(named: card.imageName)
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
         
         cell.contentConfiguration = content
+        
         
         return cell
     }
     
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailsVC = segue.destination as? EditingFlashcardViewController else {return}
+        guard let indexPath = tableView.indexPathForSelectedRow else {return}
+        let card = flashcards[indexPath.row]
+        detailsVC.flashcard = card
+    }
 }
